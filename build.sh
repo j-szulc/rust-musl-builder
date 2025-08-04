059d573847e2f245b0e7dcfbc1a130f069687754
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2025-08-04 17:47:44 +0200

    Add new test to build.sh

--- build.sh
+++ build.sh
@@ -14,3 +14,4 @@ docker buildx build \
 #   -o type=registry for pushing to docker hub
 
 docker run --rm --entrypoint sh jszulc/rust-musl-builder:bionic-$RUST_VERSION -c "cargo --version && rustc --version && rustup --version && cargo init && cargo run"
+docker run --rm -it jszulc/rust-musl-builder:bionic-$RUST_VERSION testuser 5000 sh -c "whoami && ls -lah /"
