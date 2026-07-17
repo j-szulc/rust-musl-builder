a1d8bd7fcd19df8cb2b75832c6e2f0d4ed804270
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2026-07-17 18:26:44 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ RUST_VERSION=1.88.0
 
 docker buildx build \
     --build-arg TOOLCHAIN=$RUST_VERSION \
-    --platform linux/amd64 \
+    --platform linux/arm64 \
+    --no-cache \
     -t jszulc/rust-musl-builder:bionic-$RUST_VERSION \
     -t jszulc/rust-musl-builder:latest \
     -o type=image \
     .
 #   -o type=registry for pushing to docker hub
 
-docker run --rm --entrypoint sh jszulc/rust-musl-builder:bionic-$RUST_VERSION -c "cargo --version && rustc --version && rustup --version && cargo init && cargo run"
+# docker run --rm --entrypoint sh --platform linux/arm64 jszulc/rust-musl-builder:bionic-$RUST_VERSION -c "cargo --version && rustc --version && rustup --version && cargo init && cargo run"
+docker run --rm --entrypoint sh --platform linux/amd64 jszulc/rust-musl-builder:bionic-$RUST_VERSION -c "cargo --version && rustc --version && rustup --version && cargo init && cargo run"
 docker run --rm -it jszulc/rust-musl-builder:bionic-$RUST_VERSION testuser 5000 sh -c "whoami && ls -lah / && sudo whoami"
